bf3a861e78c136441832c2ce5d6eb6b5964a11e3
Анна Левченко <levallevchenko@gmail.com>
2018-04-02 20:46:17 +0300

    Д20 - Работает без java

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
+
 var button = document.querySelector(".hotel-search");
  	var popup = document.querySelector(".search-form");
-  	var close = popup.querySelector(".hotel-search");
 	button.addEventListener("click", function (evt) {
   		 evt.preventDefault();
+  		 this.nextElementSibling 
   		popup.classList.toggle("search-form-show");
  	 });
-  	close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.toggle("search-form-show");
-  });

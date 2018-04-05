df38199e3c7b654910ed5f4bdd16140c9091c3b4
Анна Левченко <levallevchenko@gmail.com>
2018-04-05 22:55:19 +0300

    ..

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 
 var button = document.querySelector(".hotel-search");
- 	var popup = document.querySelector(".search-form");
-	button.addEventListener("click", function (evt) {
-  		 evt.preventDefault();
+ 	// var popup = document.querySelector(".search-form");
+	// button.addEventListener("click", function (evt) {
+  		 // evt.preventDefault();
   		 this.nextElementSibling 
   		popup.classList.toggle("search-form-show");
  	 });
